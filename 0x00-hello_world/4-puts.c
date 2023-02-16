fb29b7db7a227811b553fdcff6b018ab09341695
Rashnotech <rashnotech@gmail.com>
2023-02-16 08:10:23 -0800

    First C program to print

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	/* Print text to screen*/
+	/* Description: Print text to screen*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
