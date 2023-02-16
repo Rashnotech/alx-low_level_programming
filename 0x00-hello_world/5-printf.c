9780f8b7f7f9b8fd4fd9287772288920f53bdf54
Rashnotech <rashnotech@gmail.com>
2023-02-16 08:28:57 -0800

    The outcome is a piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,8 @@
  *
  * Return: an integer value 0
  */
-int main(void){
-	printf("with proper grammer, but the outcome is a piece of art,");
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
