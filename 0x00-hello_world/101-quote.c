2441b030fbe11548037e9e2c71a879fb5c043416
Rashnotech <rashnotech@gmail.com>
2023-02-16 12:07:38 -0800

    Standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints a string literal
+ *
+ * Return: an error value otherwise 0
+ */
+int main(void)
+{
+	putc("and that piece of art is useful\" - Dora Korpar, 2015-10-19",stdout);
+	return (1);
+}
