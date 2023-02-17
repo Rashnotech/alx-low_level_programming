e69b26ece29b615fe5a1dc435119a2adc60f3383
Rashnotech <rashnotech@gmail.com>
2023-02-17 09:06:51 -0800

    Numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers in base 10
+ *
+ * Return: an integer value of 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar(10);
+	return (0);
+}
