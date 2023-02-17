6c9794bafd4f4b10b22034c180522a42dd028cea
Rashnotech <rashnotech@gmail.com>
2023-02-17 09:20:05 -0800

    Print letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reserve
+ *
+ * Return: an integer value of 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
