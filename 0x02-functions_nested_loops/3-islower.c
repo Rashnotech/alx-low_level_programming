39be52d2a55c5da031fac5975aa32e079a997865
Rashnotech <rashnotech@gmail.com>
2023-02-21 06:03:02 -0800

    check if its a lowercase letter

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+/**
+ * _islower - check if its a lowercase letter
+ * @c: operand
+ *
+ * Return: an integer value 1 otherwise 0
+ */
+int _islower(int c);
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return 1;
+	else
+		return 0;
+}
