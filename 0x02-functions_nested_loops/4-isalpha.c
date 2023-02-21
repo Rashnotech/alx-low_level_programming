6c34f46e354c92f65b0ff88db880142384fecb88
Rashnotech <rashnotech@gmail.com>
2023-02-21 15:59:05 -0800

    check if its alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,10 @@
+#include "main.h"
 /**
  * _isalpha - check for alphabets
  * @c: operand
  *
  * Return: an integer value 1 otherwise 0
  */
-int _isalpha(int c);
-
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
