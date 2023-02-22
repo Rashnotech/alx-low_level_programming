870b291d001103856c3bbc71569ee1cc0108f584
Rashnotech <rashnotech@gmail.com>
2023-02-21 20:18:04 -0800

    Add two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - add to operand together
+ * @a: an operand
+ * @b: an operand
+ *
+ * Return: an integer value otherwise 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
