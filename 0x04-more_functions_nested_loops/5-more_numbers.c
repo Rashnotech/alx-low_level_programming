899186cb08606c49f2571ebeed959f2add74ed8e
Rashnotech <rashnotech@gmail.com>
2023-02-23 14:01:01 -0800

    print nestrd loop

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - print nested loop
+ *
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar(10);
+	}
+}
