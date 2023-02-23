9af674ff957a4fe046b5e8853f07b3d3c09cdd72
Rashnotech <rashnotech@gmail.com>
2023-02-23 03:10:55 -0800

    Get the product of two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - get the multiple of two integer
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: product of two integer values
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
