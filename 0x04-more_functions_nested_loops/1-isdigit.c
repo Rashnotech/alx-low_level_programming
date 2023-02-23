460884a6b2f503f0b7ca42b1740f47d3330a800d
Rashnotech <rashnotech@gmail.com>
2023-02-23 03:02:41 -0800

    Check if it is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+/**
+ * _isdigit - check if a digit is supplied
+ * @c: an operand for supplied value
+ *
+ * Return: an integer value 1 otherwise 0
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+		i++;
+	}
+}
