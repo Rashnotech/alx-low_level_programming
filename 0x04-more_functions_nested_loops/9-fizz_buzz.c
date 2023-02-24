22152852305dc98e28231e0ec175851604691a04
Rashnotech <rashnotech@gmail.com>
2023-02-23 20:35:15 -0800

    FizzBuzzing

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,24 +10,23 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
+			putchar(32);
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz");
+			putchar(32);
 		}
 		else
 		{
 			printf("%d", i);
-		}
-		if (i != 100)
-		{
 			putchar(32);
 		}
 	}
