5617e26deaeda7a4b1565b9d5ba4c739bbedf486
Rashnotech <rashnotech@gmail.com>
2023-02-27 06:40:39 -0800

    Print a string constant

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+/**
+ * _puts - print a string or character constant
+ * @str: an operand with character type
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *str; i++)
+		_putchar(*str++);
+}
