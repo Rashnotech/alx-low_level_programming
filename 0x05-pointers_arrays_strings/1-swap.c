a9fdb2b15fc35134ddc2f22aab0725c768c06935
Rashnotech <rashnotech@gmail.com>
2023-02-27 04:05:49 -0800

    Swap two integer value

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ * swap_int - exchange the value of two variable
+ * @a: an operand for pointer
+ * @b: an operand for pointer
+ * 
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	a = b;
+	b = c;
+}
