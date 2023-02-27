cecf570ec64b548ad6c1d207fa9071b1c5d8562a
Rashnotech <rashnotech@gmail.com>
2023-02-27 15:05:19 -0800

    Print string reversal

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,13 @@ void rev_string(char *s)
 	int i, len;
 	char ltemp = s[0];
 
-	len = 0, i = 0;
-	while (*s++ != '\0')
-		len++;
+	len = 0;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] != '\0')
+			len++;
+	}
+	i = 0;
 	while (i < len)
 	{
 		len--;
