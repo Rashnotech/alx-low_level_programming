9ffc676581cff5d965f60b9db39e497232376e1c
Rashnotech <rashnotech@gmail.com>
2023-02-27 15:12:35 -0800

    Print a string of character and skip each next

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * puts2 - print and skip a string
+ * @str: an operand declare as pointer/ array
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i + 2)
+	{
+		_putchar(str[i])
+	}
+	_putchar(10);
+}
