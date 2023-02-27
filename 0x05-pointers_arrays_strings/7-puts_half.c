a8ea16bc06ec9ed588ad366746b32acfc2bd7adf
Rashnotech <rashnotech@gmail.com>
2023-02-27 15:36:33 -0800

    Print half of any digit or string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,12 +15,12 @@ void puts_half(char *str)
 	}
 	if (len % 2 != 0)
 	{
-		for (i = len / 2; i <= len - 1; i++)
+		for (i = (len - 1) / 2; str[i]; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = len /2; str[i]; i++)
+		for (i = len / 2; str[i]; i++)
 			_putchar(str[i]);
 	}
 	_putchar(10);
