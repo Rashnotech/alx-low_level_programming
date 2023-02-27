bcc2d8027792ef73f4c3accedbc317212d9e9cd8
Rashnotech <rashnotech@gmail.com>
2023-02-27 15:58:44 -0800

    Print integer value with the required size

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_array - print an integer value
+ * @a: an operand with integer data type
+ * @n: an operand for size of an array
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; *str; i++)
+	{
+		if (i != n)
+		{
+			printf("%ld", *str++);
+			printf(", ");
+		}
+	}
+	_putchar(10);
+}
