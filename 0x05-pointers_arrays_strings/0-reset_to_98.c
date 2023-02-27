f740491738b23efcfb48330781da9a466241eb1e
Rashnotech <rashnotech@gmail.com>
2023-02-27 01:57:36 -0800

    Reset value to 98 using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+/**
+ * reset_to_98 - reset a value to 98
+ * @n: a pointer declaration
+ *
+ */
+void reset_to_98(int *n)
+{
+	int ptr = &n;
+
+	*ptr = 98;
+	_putchar((*ptr / 10) + '0');
+	_putchar((*ptr % 10) + '0');
+}
