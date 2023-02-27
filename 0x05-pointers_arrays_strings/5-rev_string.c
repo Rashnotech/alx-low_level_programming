9459c09b4ddfd0db0bea796aa5995a42e51cd557
Rashnotech <rashnotech@gmail.com>
2023-02-27 13:30:52 -0800

    Print string reversal

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+/**
+ * rev_string - print a string reversal
+ * @s: an operand declare as pointer/ array
+ */
+void print_rev(char *s)
+{
+	int i, len;
+
+	len = 0;
+	for (i = 0; s[i]; i++)
+	{
+		if (s[i] != '\0')
+			len++;
+	}
+	for (i = len - 1; s[i]; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar(10);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,5 +6,6 @@ void swap_int(int *a, int *b);
 int _strlen(int *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
