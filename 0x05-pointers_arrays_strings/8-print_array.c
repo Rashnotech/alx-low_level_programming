53e68dbdb38265df8e32a2b209593f819943de0d
Rashnotech <rashnotech@gmail.com>
2023-02-27 16:19:05 -0800

    Print an integer values of size n

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <string.h>
 /**
  * print_array - print an integer value
  * @a: an operand with integer data type
@@ -12,8 +11,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (strlen(a[i]) == n)
-			break;
+		if (i == (n - 1))
+		{
+			printf("%d",a[i]);
+		}
 		printf("%d", a[i]);
 		printf(", ");
 	}
