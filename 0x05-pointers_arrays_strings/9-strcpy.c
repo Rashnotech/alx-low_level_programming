2335395fbbf22cefb8d3cae7babb2830b9f79287
Rashnotech <rashnotech@gmail.com>
2023-02-27 16:46:20 -0800

    Copy string from source to destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _strcpy - copy string from source to destination
+ * @dest: an operand for the destination
+ * @src: an operand for the source
+ *
+ * Return: destinaton variable
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; *src; i++)
+		*dest[i] = *src++;
+	*dest[i] = '\0';
+}
