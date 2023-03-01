cd9dcd2afdf3e403cce9b7003a003dee7300be1e
Rashnotech <rashnotech@gmail.com>
2023-03-01 09:28:22 -0800

    Encrypt string of characters

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,11 +7,11 @@
  */
 char *rot13(char *str)
 {
-	while (*s++ != '\0')
+	while (*str++ != '\0')
 	{
 		if ((*str >= 65 && *str <= 77) || (*str >= 97 && *str <= 109))
 			str = *str + 13;
-		else
+		else if ((*str >= 78 && *str <= 90) || (*str >= 110 && *str <= 122))
 			str = *str - 13;
 	}
 	return (str);
