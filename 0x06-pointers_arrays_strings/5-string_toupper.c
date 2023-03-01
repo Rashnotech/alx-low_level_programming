cec7797bea2642c12f3795105fead3445931818a
Rashnotech <rashnotech@gmail.com>
2023-03-01 06:45:01 -0800

    Convert strings to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - convert character to uppercase
+ * @c: an operand for string of character
+ *
+ * Return: uppercase characters
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	while (*c++ != '\0')
+	{
+		if (*c++ >= 97 && *c++ <= 122)
+			*c++ += 32;
+	}
+	return (c);
+}
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * *cap_string - captalize each word in a sentence
+ * @sen: an operand for string
+ *
+ * Return: capitalize words
+ */
+char *cap_string(char *)
+{
