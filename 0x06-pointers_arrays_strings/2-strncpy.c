ae3a4da539f63f5c9914a251e187d4e059eab9b4
Rashnotech <rashnotech@gmail.com>
2023-03-01 04:25:15 -0800

    Copy a (n) number of characters

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strncpy - copy a (n) number of string
+ * @dest: an operand to contain copied string
+ * @src: an operand that hold string to be copied
+ * @n: specify the number of string to copy
+ *
+ * Return: destination operand
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len, i;
+
+	len = 0;
+	while (*src++ != '\0')
+		len++;
+	for (i = 0; i < n && *src++ != '\0'; i++)
+		dest[i] = *src++;
+	dest[i] = '\0';
+	return (dest);
+}
