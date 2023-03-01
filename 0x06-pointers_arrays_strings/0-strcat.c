2585f7bd2ba25d8801b26223f35755c4f0aab5dc
Rashnotech <rashnotech@gmail.com>
2023-03-01 02:58:40 -0800

    String concatenation function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strcat - concatenate two string characters
+ * @dest: an operand for destination string
+ * @src: an operand for source string
+ *
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+	while (*dest++ != '\0')
+		len++;
+	for (i = len - 1; *dest; i++)
+		dest[i] = *src++;
+	dest[i] = '\0';
+	return (dest);
+}
