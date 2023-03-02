7ad5e45e736ead2656dd5230360c9531ae908ec0
Rashnotech <rashnotech@gmail.com>
2023-03-02 08:17:18 -0800

    Print any number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
 /**
  * print_number - print any number
  * @n: an operand for digit
  */
 void print_number(int n)
 {
-	unsigned int i;
+	unsigned int n1;
 
-	i = n;
+	n1 = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		i = -n;
+		n1 = -n;
 	}
-	if (i / 10 != 0)
+	if (n1 / 10 != 0)
 	{
-		print_number(i / 10);
+		print_number(n1 / 10);
 	}
-	_putchar((i % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
