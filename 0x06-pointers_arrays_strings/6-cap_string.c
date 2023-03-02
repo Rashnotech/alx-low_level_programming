10321766c5320153b1dd5f788dda870a0cf8d103
Rashnotech <rashnotech@gmail.com>
2023-03-02 00:44:02 -0800

    Capitalize each word in a sentence

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,26 +9,27 @@ char *cap_string(char *str)
 	int i;
 
 	i = 0;
-	if (str[0] >= 97 && str[0] <= 122)
-		str[0] -= 32;
 	while (str[i] != '\0')
 	{
-		switch (str[i])
-		{
-			case '\n':
-			case '\t':
-			case 32:
-			case 33:
-			case 34:
-			case 40:
-			case 41:
-			case 44:
-			case 59:
-			case 63:
-			case 123:
-			case 125:
-				str[i + 1] -= 32;
-		}
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] -= 32;
+		else
+			switch (str[i])
+			{
+				case '\n':
+				case '\t':
+				case 32:
+				case 33:
+				case 34:
+				case 40:
+				case 41:
+				case 44:
+				case 59:
+				case 63:
+				case 123:
+				case 125:
+					continue;
+			}
 		i++;
 	}
 	return (str);
