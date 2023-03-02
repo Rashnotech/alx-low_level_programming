59c9ca0c0bd1fec954d86cab9d2bfe231040a6eb
Rashnotech <rashnotech@gmail.com>
2023-03-02 14:27:58 -0800

    Print an integer value

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
- * print_number - Entry point
- * Description: Prints an integer
- * @n: Integer
- * Return: int
+ * print_number - print an integer number
+ * @n: an operand for value
  */
-
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int i;
 
+	i = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		i = -n;
 	}
-
-	if ((x / 10) > 0)
-		print_number(x / 10);
-
-	_putchar((x % 10) + '0');
+	if (i / 10 != 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
 }
+
