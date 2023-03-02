0f013c73ca1d71593c623f780e4dea0e23de29bb
Rashnotech <rashnotech@gmail.com>
2023-03-02 14:17:27 -0800

    Print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,26 +1,24 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- * @n: integer params
- * Return: 0
+ * print_number - Entry point
+ * Description: Prints an integer
+ * @n: Integer
+ * Return: int
  */
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
+	unsigned int x = n;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n;
+		x = -x;
 	}
 
-	if ((n1 / 10) > 0)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) + '0');
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar((x % 10) + '0');
 }
