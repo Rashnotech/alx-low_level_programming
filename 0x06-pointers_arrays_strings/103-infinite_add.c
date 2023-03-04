77d4025af244dcee4489eb148a7eec7ca02f8eff
Rashnotech <rashnotech@gmail.com>
2023-03-04 07:28:06 -0800

    Addition of two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,3 +1,5 @@
+int _strlen(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
  * _strlen - find the string length of a string
  * @n: an operand for input character
@@ -9,8 +11,8 @@ int _strlen(char *n)
 	int len;
 
 	for (len = 0; *n != '\0'; n++)
-		len++;	
-	return len;
+		len++;
+	return (len);
 }
 /**
  * *infinite_add - add two numbers together of type char
@@ -23,13 +25,13 @@ int _strlen(char *n)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int counter; i, j, len1, len2;
+	int counter, i, j, len1, len2;
 
 	counter = 0;
 	len1 = _strlen(n1);
 	len2 = _strlen(n2);
 	if (len1 >= size_r || len >= size_r || counter >= size_r)
-		return 0;
+		return (0);
 	i = len1 - 1;
 	j = len2 - 1;
 	while (i >= 0 || j >= 0 || counter >= 0)
@@ -46,11 +48,11 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			j--;
 		}
 		counter = sum / 10;
-		if(_strlen(r) < size_r - 1)
+		if (_strlen(r) < size_r - 1)
 			*r++ = sum % 10 + '0';
 		else
-			return 0;
+			return (0);
 	}
 	*r = '\0';
-	return r;
+	return (r);
 }
