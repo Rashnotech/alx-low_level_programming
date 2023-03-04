c8b905729653b2b712bffdd91c86406dc8e8fc1f
Rashnotech <rashnotech@gmail.com>
2023-03-04 14:15:10 -0800

    Addition of two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 int _strlen(char *n);
+void _reverse(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 /**
  * _strlen - find the string length
@@ -14,7 +15,23 @@ int _strlen(char *n)
 		len++;
 	return (len);
 }
+/**
+ * _reverse - reverse a string of characters
+ * @str: an operand for pointer
+ */
+void _reverse(char *str)
+{
+	int len, i;
+	char temp;
 
+	len = _strlen(str);
+	for (i = 0; i <= len / 2; i++)
+	{
+		temp = str[len - i - 1];
+		str[len - i - 1] = temp;
+		str[i] = temp;
+	}
+}
 /**
  * *infinite_add - add two numbers together of type char
  * @n1: an operand of the first number
@@ -26,37 +43,32 @@ int _strlen(char *n)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int counter, i, j, sum, len1, len2;
+	int counter, i, j, max, sum, len1, len2;
 
-	counter = 0;
+	counter = 0, max = 0;
 	len1 = _strlen(n1);
 	len2 = _strlen(n2);
 	if (len1 >= size_r || len >= size_r)
 		return (0);
 	i = len1 - 1;
 	j = len2 - 1;
-	while (i >= 0 || j >= 0 || counter >= 0)
+	while (i >= 0 || j >= 0 || counter > 0)
 	{
 		sum = counter;
 		if (i >= 0)
-		{
 			sum += n1[i] - '0';
-			i--;
-		}
 		if (j >= 0)
-		{
 			sum += n2[j] - '0';
-			j--;
-		}
 		counter = sum / 10;
-		if (_strlen(r) < size_r - 1)
-			(*r)++ = sum % 10 + '0';
+		if (_strlen(r) < (size_r - 1))
+			r[max] = (sum % 10) + '0';
 		else
 			return (0);
+		i--;
+		j--;
+		max++;
 	}
-	if (counter != 0)
-		return (0);
-
-	*r = '\0';
+	r[max + 1] = '\0';
+	_reverse(r);
 	return (r);
 }
