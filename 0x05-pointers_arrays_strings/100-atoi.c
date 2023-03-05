3f22c2ef89176c763a830d8b8680fb81ba6ef4dd
Rashnotech <rashnotech@gmail.com>
2023-03-05 14:05:35 -0800

    convert string to digit

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,24 +11,16 @@ int _atoi(char *s)
 	unsigned int num;
 
 	sign = 1, i = 0, num = 0;
-	while (s[i] == ' ')
-		i++;
-	if (s[i] == '-')
+	while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
 	{
-		sign *= -1;
+		if (s[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	else if (s[i] == '+')
-		i++;
-	while (s[i] != '\0')
+	i = 0;
+	while (s[i] >= '0' && s[i] <= '9')
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			num = num * 10 + digit;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-		}
+		num = num * 10 + (s[i] - '0');
 		i++;
 	}
 	return (num * sign);
