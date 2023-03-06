a989c1b9ecbb77892590a86525948cd24c7205a9
Rashnotech <rashnotech@gmail.com>
2023-03-06 06:28:29 -0800

    Filled the buffer with const value

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+ * _memset - fill in a memory with constant bytes
+ * @s: memory area of the buffer
+ * @b: const value to be written
+ * @n: size of const value to written in memory
+ *
+ * Return: pointer of the memory s or buffer
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n != 0)
+	{
+		s = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+#More on pointer and array task
+Doing hard things is the mindset and Learning is the goal
+Finding the missing piece of puzzle
