8a5f30f54b1877be025ea8d167d34ad4be34cdc7
Rashnotech <rashnotech@gmail.com>
2023-03-06 07:50:56 -0800

    Search character from a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ * _strchr - finds if a character is in a string
+ * @s: string of character to make search
+ * @c: character to check find
+ *
+ * Return: found string otherwise NULL;
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
