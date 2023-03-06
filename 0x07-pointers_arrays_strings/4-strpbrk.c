db9438ddaa91660c61aefc328d86c1fe986bc192
Rashnotech <rashnotech@gmail.com>
2023-03-06 13:53:55 -0800

    Find a matching character of a substring from a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+/**
+ * _strbrk - find the match character of accept in a string
+ * @s: string of characters
+ * @accept: substring of char
+ *
+ * Return: character found during search otherwise NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		while (*accept != '\0')
+		{
+			if (*s == *accept)
+				return (s);
+			accept++;
+		}
+		s++;
+	}
+	return (0);
+}
+
