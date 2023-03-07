36df4daccc18837096902bc560be65ea7b0584a1
Rashnotech <rashnotech@gmail.com>
2023-03-06 16:16:42 -0800

    Print the sum two diagonal matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,17 @@
+/**
+ * print_diagsums - progam that print two diagonal matrix
+ * @a: a matrix
+ * @size: size of the matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, i;
+
+	sum1 = 0; sum = 2;
+	for (i = 0; i < size ; i++)
+	{
+		sum1 += *(a + i * size + i);
+		sum2 += *(a + i * size + size - 1 - i);
+		printf("%d,%d\n", sum1, sum2);
+	}
+}
