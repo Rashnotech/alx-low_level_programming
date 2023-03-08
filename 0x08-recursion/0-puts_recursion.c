7e342ad63aa48d670c636a8ebb6216d26ad14a3c
Rashnotech <rashnotech@gmail.com>
2023-03-08 04:41:12 -0800

    Print recursively a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+/**
+ * _puts_recursion - a function that print a string recursively
+ * @s: string of characters
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar(10);
+	_putchar(_puts_recursion(*s));
+	s++;
+}
