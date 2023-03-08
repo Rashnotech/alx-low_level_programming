a048b5968d7862bb1381ae75dfdd3831ab226c10
Rashnotech <rashnotech@gmail.com>
2023-03-08 13:33:21 -0800

    Power of any value

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -12,9 +12,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y > 0)
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 	else
 	{
-		y--;
-		return (x * _pow_recursion(x, y));
+		return (1 / _pow_recursion(x, -y));
 	}
 }
