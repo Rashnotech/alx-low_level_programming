ed3c8d65e90866791945f6873fbb3a9d37e9a3e9
Rashnotech <rashnotech@gmail.com>
2023-03-08 06:08:03 -0800

    Get string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - get the string length of any string
+ * @s: string of character
+ *
+ * Return: length of the string otherwise 0
+ */
+int _strlen_recursion(char *s)
+{
+	int len;
+	
+	len = 0
+	if (*s == '\0')
+		return (len);
+	len++;
+	_strlen_recursion(s + 1);
+}
