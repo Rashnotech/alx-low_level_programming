ef9469ed88414770093956bb88e0ed25e46d581f
Rashnotech <rashnotech@gmail.com>
2023-03-08 07:03:54 -0800

    String length of character

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len;
-
-	len = 0;
 	if (*s == '\0')
 		return (0);
-	len++;
-	_strlen_recursion(s + 1);
-	return (len);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
