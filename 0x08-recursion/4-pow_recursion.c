4762ad3171abaea9ed2cd7dc3314390d7679f228
Rashnotech <rashnotech@gmail.com>
2023-03-08 13:12:06 -0800

    Power of any value

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that gets the power of values
+ * @x: any positive integer number
+ * @y: power of the operand
+ *
+ * Return: an integer value otherwise -1
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
