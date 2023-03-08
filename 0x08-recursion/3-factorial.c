0972641fd1bf8c6d450fd980a4005f1d5318a5ed
Rashnotech <rashnotech@gmail.com>
2023-03-08 07:18:09 -0800

    Factorial of any number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * factorial - Factorial of any number
+ * @n: an integer value
+ *
+ * Return: factorial of the number otherwise -1
+ */
+int factorial(int n)
+{
+	if ( n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
