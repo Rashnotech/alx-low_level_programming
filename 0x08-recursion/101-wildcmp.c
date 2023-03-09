6b8a0cde9f746c5e63aa61932d22cea9b0feba21
Rashnotech <rashnotech@gmail.com>
2023-03-09 06:38:55 -0800

    compare two strings for match

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,12 +15,9 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '*')
 	{
 		if (*s2 != '\0' && (wildcmp(s1, s2 + 1)))
-		{
 			return (1);
-		}
 		if (wildcmp(s1 + 1, s2))
 			return (1);
-		return (wildcmp(s1 + 1, s2));
 	}
 	return (0);
 }
