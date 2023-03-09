65bb3d934116c34b83c252fa21560b2ec27d9967
Rashnotech <rashnotech@gmail.com>
2023-03-09 00:25:33 -0800

    Prime numbers check

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,32 +1,44 @@
 #include "main.h"
-
-int actual_prime(int n, int i);
-
 /**
- * is_prime_number - says if an integer is a prime number or not
- * @n: number to evaluate
+ * is_prime_helper - prime helper function
+ * @n: accept any number
+ * @i: iterator
  *
- * Return: 1 if n is a prime number, 0 if not
+ * Return: an integer value of 1 othewise 0
+ */
+int is_prime_helper(int n, int i)
+{
+	if (n <= 2)
+	{
+		return ((n == 2) ? 1 : 0);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else if (i * i > n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_helper(n, i + 1));
+	}
+}
+/**
+ * is_prime_number - Check if a number is prime
+ * @n: any integer value
+ *
+ * Return: an integer value of 1 othewise 0
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
+	if (n < 2)
+	{
 		return (0);
-	return (actual_prime(n, n - 1));
-}
-
-/**
- * actual_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- *
- * Return: 1 if n is prime, 0 if not
- */
-int actual_prime(int n, int i)
-{
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (actual_prime(n, i - 1));
+	}
+	else
+	{
+		return (is_prime_helper(n, 2));
+	}
 }
