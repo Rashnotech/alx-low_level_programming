319c3b9d577776684a59c2702e973e6d403d257b
Rashnotech <rashnotech@gmail.com>
2023-03-09 05:59:03 -0800

    compare two strings for match

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,46 +1,26 @@
 #include "main.h"
-
 /**
- * check_match - Entry point
- * Description: Checking for match
- * @s1: Character
- * @s2: Character
- * Return: 1 or 0
+ * wildcmp - compare two string of characters for match
+ * @s1: first string compare
+ * @s2: second string compare
+ *
+ * Return: an integer value 1, otherwise 0
  */
-
-int check_match(char *s1, char *s2)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-	{
-		return (1);
-	}
-	if (*s1 == *s2)
-	{
-		return (check_match(s1 + 1, s2 + 1));
-	}
-	if (*s2 == '*')
-	{
-		if (check_match(s1, s2 + 1))
-		{
-			return (1);
-		}
-		if (*s1 != '\0' && check_match(s1 + 1, s2))
-		{
-			return (1);
-		}
-	}
-	return (0);
-}
-
-/**
- * wildcmp - Entry point
- * Description: Compares 2 strings
- * @s1: Character
- * @s2: Character
- * Return: 1 or 0
- */
-
 int wildcmp(char *s1, char *s2)
 {
-	return (check_match(s1, s2));
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == '*')
+	{
+		while (*s2 != '\0')
+		{
+			if ((wildcmp(s1 + 1, s2) == 1)
+					return (1);
+			s2++;
+		}
+		return (s1 + 1, s2);
+	}
+	return (0);
 }
