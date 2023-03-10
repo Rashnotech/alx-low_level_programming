87001b21d1926a07314b4fcb85d81e729f2fb038
Rashnotech <rashnotech@gmail.com>
2023-03-10 13:51:07 -0800

    Get the number of argument passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,12 +8,6 @@
  */
 int main(int argc, char *argv[])
 {
-	int count;
-
-	if (argc > 1)
-	{
-		for (count = 1; count < argc; count++)
-			printf("%s\n", argv[count]);
-	}
+	printf("%d\n", argc);
 	return (0);
 }
