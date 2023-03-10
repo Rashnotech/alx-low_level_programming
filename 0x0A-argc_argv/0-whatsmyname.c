f64a82af461279e843b1f2e1cd80f32552d7a75c
Rashnotech <rashnotech@gmail.com>
2023-03-10 13:46:51 -0800

    Print name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,6 +8,13 @@
  */
 int main(int argc, char *argv[])
 {
-	printf("%c\n", argv[0]);
+	int count;
+
+	printf("%s\n", argv[0]);
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+			printf("%s\n", argv[count]);
+	}
 	return (0);
 }
