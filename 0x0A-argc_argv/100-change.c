2f9b1d54fef771dbbc595b793772e617c12d1896
Rashnotech <rashnotech@gmail.com>
2023-03-12 04:16:47 -0700

    Change cents into denomination

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
 	int coins[] = {25, 10, 5, 2, 1};
 	int rem, i, result, num;
 
-	num = atoi(argv[1]);
 	result = 0, i = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num = atoi(argv[1]);
 	if (num < 0)
 		printf("0\n");
 
