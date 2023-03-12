be4e5bab62e17c1d7deac812ecbc372c6bc7d7f8
Rashnotech <rashnotech@gmail.com>
2023-03-12 04:27:32 -0700

    Multiplication of two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,16 @@ int main(int argc, char *argv[])
 	int counter, mult;
 
 	mult = 1;
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 	if (argc >= 2 && argc <= 3)
 	{
 		for (counter = 1; counter <= 2; counter++)
 			mult *= atoi(argv[counter]);
 		printf("%d\n", mult);
-	}
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
 	}
 	return (0);
 }
