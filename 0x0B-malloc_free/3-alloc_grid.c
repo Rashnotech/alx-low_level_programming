2bc2ba6eca79d981503471937a6f9139dd38b11c
Rashnotech <rashnotech@gmail.com>
2023-03-13 15:17:39 -0700

    Multidimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-			ptr_int[j] = 0;
+			ptr_int[i][j] = 0;
 	}
-	return (**ptr_int);
+	return (ptr_int);
 }
