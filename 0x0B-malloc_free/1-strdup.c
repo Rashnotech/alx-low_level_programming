9908d1c1b6b8c6f5cfa6797cb76e64c1984c852a
Rashnotech <rashnotech@gmail.com>
2023-03-13 13:44:41 -0700

    Copy string of characters;

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - copy a string given as a parameter
+ * @str: string literal
+ *
+ * Return: characters otherwise NULL
+ */
+char *_strdup(char *str)
+{
+	char *ptr_str;
+	int i;
+
+	i = 0;
+	if (*str == NULL)
+		return (NULL);
+	ptr_str = malloc(sizeof(str) + 1);
+	while (*str != '\0')
+	{
+		ptr_str[i] = *str++;
+		i++;
+	}
+	ptr_str[i] = '\0';
+	return (ptr_str);
+}
