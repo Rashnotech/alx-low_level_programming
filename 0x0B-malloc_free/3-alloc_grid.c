473ae9b80f38d4c53e229fbe6b3c2c356ba40517
Rashnotech <rashnotech@gmail.com>
2023-03-14 02:12:20 -0700

    Multi-dimensional array

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,6 @@ int **alloc_grid(int width, int height)
 	ptr_int = malloc(height * sizeof(int *));
 	if (ptr_int == NULL)
 	{
-		free(ptr_int);
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
@@ -25,7 +24,9 @@ int **alloc_grid(int width, int height)
 		ptr_int[i] = malloc(sizeof(int) * width);
 		if (ptr_int[i] == NULL)
 		{
-			free(ptr_int[i]);
+			for (j = 0; j < i; j++)
+				free(ptr_int[j]);
+			free(ptr_int);
 			return (NULL);
 		}
 		for (j = 0; j < width; j++)
