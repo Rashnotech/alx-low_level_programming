79a64ce6e7d3f2cff451407b012f64980a4da97c
Rashnotech <rashnotech@gmail.com>
2023-03-14 02:32:40 -0700

    Freeing a 2Dimensional array

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -13,7 +13,7 @@ void free_grid(int **grid, int height)
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < height; j++)
-			free(grid[i][j]);
+			free(grid[j]);
 		free(grid[i]);
 	}
 	free(grid);
