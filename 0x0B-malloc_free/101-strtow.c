aaf3b33901ab1f82ff0c96f1bc45120781da6bf5
Rashnotech <rashnotech@gmail.com>
2023-03-15 03:05:42 -0700

    Convert string to words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * strtow - convert a string into word
+ * @str: a string literal
+ *
+ * Return: a each word otherwise null
+ */
+char **strtow(char *str)
+{
+	char **words;
+	int i, j, num_word;
+
+	if (*str == NULL || *str == ' ')
+		return (NULL);
+	while (*str != '\0')
+	{
+		if (*str == ' ' || *str == '\t')
+			continue;
+		num_word++;
+		str++;
+	}
+	words = malloc(num_words + 1);
+	if (words == NULL)
+		return (NULL);
+	for (i = 0; *str; i++)
+	{
+		words[i] = malloc(num_words + 1);
+		if (words[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(words[j]);
+			}
+			return (NULL);
+			free(words);
+		}
+		if (str[i] != ' ' || str[i] != '\t' && (str[i - 1] == ' '))
+		{
+			for (j = 0; j < num_words; j++)
+				words[i][j] = str[i];
+		}
+	}
+	words[num_words + 1] = NULL;
+	return (words);
+}
