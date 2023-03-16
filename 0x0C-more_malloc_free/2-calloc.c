be184c46558fdcf5cfeec9a554bd132e23cfccf8
Rashnotech <rashnotech@gmail.com>
2023-03-16 10:16:06 -0700

    Creating a memory allocation using calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -6,14 +6,20 @@
  * _calloc - a function that allocates memory using malloc
  * @nmemb: an array of memory bytes
  * @size: size of memory to be allocated
+ *
+ * Return: a memory allocated
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
+
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	void *mem = malloc(nmemb * size);
+
 	if (mem == NULL)
 		return (NULL);
-	memset(mem, 0, nmemb * size);
+	for (i = 0; i < (nmemb * size); i++)
+		mem[i] = 0;
 	return (mem);
 }
