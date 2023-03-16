bd156768de3f22c454d846950efcd2898aa1ae08
Rashnotech <rashnotech@gmail.com>
2023-03-16 14:42:41 -0700

    Create a range of integer from min to max

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,13 +3,17 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * array_range - an array 
+ * array_range - an array range from min to max
+ * @min: minimun integer to start
+ * @max: maximum integer to end loop
+ *
+ * Return: an array of integers otherwise NULL
  */
 int *array_range(int min, int max)
 {
 	int *range, i;
 
-	if (min > max)
+	if (min >= max)
 		return (NULL);
 	range = malloc(sizeof(int) * (max + 1));
 	if (range == NULL)
