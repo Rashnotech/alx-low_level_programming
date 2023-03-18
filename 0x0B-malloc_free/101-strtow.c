b86c8f6d14e63c4994ebdf78340936d19fd67394
Rashnotech <rashnotech@gmail.com>
2023-03-18 03:13:03 -0700

    String literal to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -39,8 +39,8 @@ char **strtow(char *str)
 		}
 		new_word = malloc(sizeof(char) * (word_length + 1));
 		memcpy(new_word, &str[word_start], word_length);
-		new_word[word_length] = '\0';
-		words[word_index + 1] = new_word;
+		new_word[word_length + 1] = '\0';
+		words[word_index++] = new_word;
 	}
 	words[word_index] = NULL;
 	return (words);
