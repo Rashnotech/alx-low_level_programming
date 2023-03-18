873f6854f50066a023c4d5e5613db67fdaef9362
Rashnotech <rashnotech@gmail.com>
2023-03-18 04:35:36 -0700

    String Literal to word

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -32,7 +32,7 @@ char **strtow(char *str)
 			continue;
 		word_start = i;
 		word_length = 0;
-		while (str[i] && str[i] != ' ' && i != 0)
+		while (str[i] && str[i] != ' ')
 		{
 			word_length++;
 			i++;
