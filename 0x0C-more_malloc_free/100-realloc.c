9545084b43eda536392d6e1ebb7f7a79c595cee6
Rashnotech <rashnotech@gmail.com>
2023-03-17 22:25:25 -0700

    Reallocataing memory when old_size is not enough

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * _realloc - reallocate a new block
+ * @ptr: a block of memory
+ * @old_size: previous memory allocated
+ * @new_size: New memory to be allocated
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_mem;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_mem = malloc(new_size);
+	if (new_mem == NULL)
+		return (NULL);
+	if (ptr != NULL)
+	{
+		memcpy(new_mem, ptr, old_size);
+		free(ptr);
+	}
+	return (new_mem);
+}
