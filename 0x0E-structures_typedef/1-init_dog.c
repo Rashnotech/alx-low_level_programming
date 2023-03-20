e92bfbc3dc97cc3a5375691a475c94dbd2f16b30
Rashnotech <rashnotech@gmail.com>
2023-03-20 14:39:55 -0700

    Initializing the struct declaration

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - Initializing struct (dog)
+ * @d: an address for struct pointer
+ * @name: member name
+ * @age: member age
+ * @owner: member owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
