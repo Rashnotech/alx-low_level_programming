0434c4b14f3528c85dc7976fe758a892f4a564a8
Rashnotech <rashnotech@gmail.com>
2023-03-20 14:21:18 -0700

    Struct definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - attribute (member) of dog
+ * @name: First member
+ * @age: second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
