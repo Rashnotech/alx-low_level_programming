382467a90a185dd2bf65b5c1d26fb5a76a14b4ff
Rashnotech <rashnotech@gmail.com>
2023-03-22 10:09:43 -0700

    Print a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - A function that prints a name
+ * @name: a name of a person
+ * f: a function that takes in an array of character
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
