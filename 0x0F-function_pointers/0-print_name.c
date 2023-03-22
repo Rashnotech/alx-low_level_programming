6de6a779743939d55c9afe8dcf2956f9fed347f6
Rashnotech <rashnotech@gmail.com>
2023-03-22 10:18:47 -0700

    Print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - A function that prints a name
  * @name: a name of a person
- * f: a function that takes in an array of character
+ * @f: a function that takes in an array of character
  */
 void print_name(char *name, void (*f)(char *))
 {
