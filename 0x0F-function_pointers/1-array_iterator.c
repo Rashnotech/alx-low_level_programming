034e1e7a22bb2cc7d351f995c829686a568645d9
Rashnotech <rashnotech@gmail.com>
2023-03-22 21:36:08 -0700

    An array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL)
+	if (array == NULL || action == NULL)
 		return;
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
-	
 }
