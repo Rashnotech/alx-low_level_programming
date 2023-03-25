17594a8837dfab679d542695f8ca36d26e5c617a
Rashnotech <rashnotech@gmail.com>
2023-03-25 10:35:49 -0700

    String to words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -17,7 +17,8 @@ char **strtow(char *str)
 	word_count = 0;
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] != ' ' && (i == 0 || str[i + 1] == ' '))
+		if ((str[i] != ' ' || str[i] != '\t') &&
+		(str[i + 1] != ' ' && str[i + 1] != '\0'))
 			word_count++;
 	}
 	if (!word_count)
