92bd2d21dd651c5eed9d9606f8bc3a6777e50dbb
Rashnotech <rashnotech@gmail.com>
2023-03-30 14:04:12 -0700

    Prints before the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * print_literal - Prints a string literal before main
+ */
+void __attribute__((constructor)) print_literal(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
