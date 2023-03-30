f848d1bb887c3a46fcb6071a47b663a51bf5bc8e
Rashnotech <rashnotech@gmail.com>
2023-03-30 09:15:35 -0700

    Check the number of element in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * list_len - find the number of element in a list
+ * @h: struct pointer
+ * 
+ * Return: number of length size_t type
+ */
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != (list_t *) 0)
+	{
+		h = h->next;
+		counter++;
+	}
+	return (counter);
+}
