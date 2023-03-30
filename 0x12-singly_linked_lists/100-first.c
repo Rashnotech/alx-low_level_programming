28a9d7cadbcde51317eab5b83558a26f96e69b52
Rashnotech <rashnotech@gmail.com>
2023-03-30 14:12:33 -0700

    Prints before the main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,6 +4,6 @@
  */
 void __attribute__((constructor)) print_literal(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	"I bore my house upon my back!\n");
 }
