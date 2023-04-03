e0f16a8193705b5553e50818466dd1948603d21c
Rashnotech <rashnotech@gmail.com>
2023-04-03 15:16:24 -0700

    Free a pointer struct list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_listint2 - Free an allocate memory
+ * @head: a struct pointer
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
