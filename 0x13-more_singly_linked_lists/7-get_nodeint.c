31bb1c79cd2ab464f850e8ce9366855d3470ac9c
Rashnotech <rashnotech@gmail.com>
2023-04-03 16:32:41 -0700

    Get node indexed element

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - get a node at the specified index
+ * @head: pointer to the start of node
+ * @index: specified index
+ *
+ * Return: a struct type element
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+
+	while (head != NULL)
+	{
+		head = head->next;
+		if (counter == index - 1)
+			return (head);
+		counter++;
+	}
+	return (NULL);
+}
