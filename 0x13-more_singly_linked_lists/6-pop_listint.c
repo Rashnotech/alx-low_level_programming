4d36fe70f8ea7970a7c481dc3f7d2de3499263b5
Rashnotech <rashnotech@gmail.com>
2023-04-03 16:12:39 -0700

    Pop the first element in a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * pop_listint - pop the first element on the list
+ * @head: a double pointer to the struct
+ *
+ * Return: an integer value 0 or pop node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *next_val; 
+	int pop_val;
+
+	if (*head == NULL)
+		return (0);
+	pop_val = (*head)->n;
+	next_val = (*head)->next;
+	free(*head);
+	*head = next_val;
+	return (pop_val);
+}
