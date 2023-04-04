083e02f056128b0587453054c39e98ae28523d51
Rashnotech <rashnotech@gmail.com>
2023-04-04 13:38:46 -0700

    Insert in any part of the linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,8 +13,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *temp;
 	unsigned int counter;
 
-	if(!head)
-		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
@@ -22,9 +20,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = NULL;
 	temp = *head;
 	counter = sizeoflist(*head);
-	if (idx > counter)
-		return (NULL);
 	counter = 0;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+	}
 	while (counter < idx - 1)
 	{
 		temp = temp->next;
