e10e6f814bf97d5b4cc4b9e7be634d69d31f092b
Rashnotech <rashnotech@gmail.com>
2023-04-04 10:27:28 -0700

    Insert node in any position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+int sizeoflist(listint_t *head);
 /**
  * insert_nodeint_at_index - insert node element at specified index
  * @head: double ponter to list
@@ -10,7 +11,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *temp;
-	unsigned int counter = 0;
+	unsigned int counter;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node || !head)
@@ -18,14 +19,35 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	temp = *head;
+	counter = sizeoflist(*head);
+	if (idx > counter)
+		return (NULL);
+	counter = 0;
 	while (counter < idx - 1)
 	{
 		temp = temp->next;
 		counter++;
 	}
-	if (counter > idx)
-		return (NULL);
 	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
+}
+
+/**
+ * sizeoflist - get the size of the list
+ * @head: struct pointer
+ *
+ * Return: the size of the list
+ */
+int sizeoflist(listint_t *head)
+{
+	unsigned int counter;
+
+	counter = 0;
+	while (head != NULL)
+	{
+		head = head->next;
+		counter++;
+	}
+	return (counter);
 }
