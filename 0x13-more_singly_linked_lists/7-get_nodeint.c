7094afc0f4333d28eec52282c4f97d725250ca0f
Rashnotech <rashnotech@gmail.com>
2023-04-04 13:23:52 -0700

    Get the node index of an element

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,6 +10,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int counter = 0;
 
+	if (!head)
+		return (NULL);
 	while (head != NULL)
 	{
 		head = head->next;
