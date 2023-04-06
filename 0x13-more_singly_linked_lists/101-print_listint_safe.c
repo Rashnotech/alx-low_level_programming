888e4d5d32b96404af14442e7f7bd9f1d64ce081
Rashnotech <rashnotech@gmail.com>
2023-04-05 17:46:58 -0700

    Print  linked list without repitition

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdbool.h>
+/**
+ * print_listint_safe - Print node list in safe
+ * @head: a pointer to struct
+ *
+ * Return: an unsigned integer of element contained in node otherwise exit 98
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t counter = 0;
+
+	if (!head || !head->next)
+		exit(98);
+	while (head)
+	{
+		printf("[%p] %d\n",(void *) head, head->n);
+		if (head > head->next)
+		{
+			head = head->next;
+		}
+		else
+		{
+			head = head->next;
+			printf("->[%p] %d\n", (void *) head, head->n);
+			break;
+		}
+		counter++;
+	}
+	return (counter);
+}
