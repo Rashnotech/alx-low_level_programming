1761a4d3ae79e8cbd829fd45fcb53d7025182e6c
Rashnotech <rashnotech@gmail.com>
2023-04-07 12:46:55 -0700

    Find the loop in the linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * find_listint_loop - find the begining of the list in a loop
+ * @head: a pointer to pointer to list
+ *
+ * Return: struct node
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *start_node, *end_node;
+
+	start_node = end_node = head;
+	if (!head || !head->next)
+		return (NULL);
+	while (end_node && end_node->next)
+	{
+		start_node = start_node->next;
+		end_node = end_node->next->next;
+		if (start_node == end_node)
+		{
+			start_node = head;
+			while (start_node != end_node)
+			{
+				start_node = start_node->next;
+				end = end_node->next;
+			}
+			return (start_node);
+		}
+	}
+	return (NULL);
+}
