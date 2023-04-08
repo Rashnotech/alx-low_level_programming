d269e8b8352cb4d1abc8c6177dc7c7bf4216e4c7
Rashnotech <rashnotech@gmail.com>
2023-04-08 09:07:41 -0700

    Get bit index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int shift_num, bit_val;
 
-	if (!n || !index)
+	if (!n)
 		return (-1);
-
 	shift_num = n >> index;
 	bit_val = shift_num & 1;
 	return (bit_val);
