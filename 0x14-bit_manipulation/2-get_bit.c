217d2741a81c91da078c96ed4dacfb2967e7d17c
Rashnotech <rashnotech@gmail.com>
2023-04-08 09:02:59 -0700

    Get bit index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * get_bit - get the bit from a specific index
+ * @n: number to check index from
+ * @index: specific index to find
+ *
+ * Return: an index otherwise -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int shift_num, bit_val;
+
+	if (!n)
+		return (-1);
+
+	shift_num = n >> index;
+	bit_val = shift_num & 1;
+	return (bit_val);
+}
