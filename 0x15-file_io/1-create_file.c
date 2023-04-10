4725373f39c0a67e5553c2b5c2e0443b68cb0527
Rashnotech <rashnotech@gmail.com>
2023-04-10 16:48:14 -0700

    Create file with file permission

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,13 +20,14 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-		text_content = " ";
-	wd = write(fd, text_content, strlen(text_content));
-	if (wd == -1)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
+		wd = write(fd, text_content, strlen(text_content));
+		if (wd == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
