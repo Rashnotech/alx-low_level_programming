3531712a4271d5d14329d96bd3c5edadd3a15244
Rashnotech <rashnotech@gmail.com>
2023-04-10 16:41:45 -0700

    Read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buff = malloc(letters + 1);
+	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 	{
 		close(fd);
