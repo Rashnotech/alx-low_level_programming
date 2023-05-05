3c7ad6022091999851ae25333455f3cf7e02d396
Rashnotech <rashnotech@gmail.com>
2023-05-05 16:01:23 -0700

    Flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * flip_bits - flip bits from one number to another
+ * @n: starting point
+ * @m: destination
+ *
+ * Return: an integer value otherwise 0
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m;
+	unsigned int index;
+
+	index = 0;
+	while (x)
+	{
+		index += ((n ^ m) & 1);
+		x = (n ^ m) >> 1;
+	}
+	return (index);
+}
