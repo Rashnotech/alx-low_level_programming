64cb579a63b1b9a87e20cb8ba192f1328a7ec657
Rashnotech <rashnotech@gmail.com>
2023-05-08 16:35:59 -0700

    Create a file and write inside the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,7 @@
  * create_file - a function that creates a file
  * @filename: the name of the file to create
  * @text_content: content to write in file
- * 
+ *
  * Return: an integer value of 1 otherwise -1
  */
 int create_file(const char *filename, char *text_content)
@@ -14,14 +14,15 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (!filename || fd == -1)
 		return (-1);
-	if (!text_content)
-		return (-1);
-	x_len = strlen(text_content);
-	nwrite = write(fd, text_content, x_len);
-	if (nwrite == -1)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
+		x_len = strlen(text_content);
+		nwrite = write(fd, text_content, x_len);
+		if (nwrite == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 	close(fd);
 	return (1);
