65258d9440e1371111e3bc12305021e3dc6bca28
Rashnotech <rashnotech@gmail.com>
2023-05-10 06:07:33 -0700

    Function prototypes and Headers

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,4 +10,7 @@ int _putchar(char *c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void checkfile(int file_from, int file_to, char **arr);
+void print_error(char *str, int code, char *file);
+void copy_to_file(char **arr);
 #endif
