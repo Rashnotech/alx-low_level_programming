97dab9ce97c47bf57bdb627616a0a95cdf2d48e3
Rashnotech <rashnotech@gmail.com>
2023-06-16 14:56:42 +0000

    Delete a node at specific index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -6,18 +6,24 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	int len, counter = 1;
+	unsigned int len, counter = 1;
 	dlistint_t *temp, *prev = NULL;
 
 	temp = *head;
 	len = get_node_size(*head);
 	if (index > len || head == NULL)
 		return (-1);
-	if (index == 0)
+	if (index == 0 && len != 0)
 	{
 		*head = (*head)->next;
-		free((*head)->prev);
+		free(temp);
+		temp = NULL;
 		(*head)->prev = NULL;
+	}
+	else if (len == 1)
+	{
+		free(temp);
+		temp = NULL;
 	}
 	else
 	{
@@ -33,11 +39,35 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			free(temp);
 			temp = NULL;
 		}
-		prev = temp->prev;
-		prev->next = temp->next;
-		temp->next->prev = prev;
-		free(temp);
-		temp = NULL;
+		else
+		{
+			prev = temp->prev;
+			prev->next = temp->next;
+			temp->next->prev = prev;
+			free(temp);
+			temp = NULL;
+		}
 	}
 	return (1);
 }
+
+
+/**
+ * get_node_size - Get the size of a struct node
+ * @head: struct node pointer of list
+ * Return: an integer of node size
+ */
+int get_node_size(dlistint_t *head)
+{
+	dlistint_t *temp;
+	int counter = 0;
+
+	temp = head;
+	while (temp && temp->next)
+	{
+		counter++;
+		temp = temp->next;
+	}
+	return (counter);
+}
+
