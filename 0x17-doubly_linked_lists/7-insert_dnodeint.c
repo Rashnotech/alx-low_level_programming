b145e9348fa77ebe1e73571442fc9bfbb9ee9452
Rashnotech <rashnotech@gmail.com>
2023-06-16 23:31:47 +0000

    Fixed Error: Check 5 & 6

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,30 +27,32 @@ int get_node_size(dlistint_t *head)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *xx_next, *temp;
+	dlistint_t *new_node, *xx_next = NULL, *temp;
 	unsigned int len;
 
-	xx_next = *h;
+	temp = *h;
 	len = get_node_size(*h);
 	if (idx > len)
 		return (NULL);
-	if (idx == 0 || h == NULL)
+	if (idx == 0 || *h == NULL)
 		return (add_dnodeint(h, n));
 	while (idx != 1)
 	{
-		xx_next = xx_next->next;
+		temp = temp->next;
 		idx--;
 	}
-	if (xx_next->next == NULL)
+	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
+	xx_next = temp;
 	temp = xx_next->next;
-	xx_next->next = new_node;
-	temp->prev = new_node;
-	new_node->next = temp;
 	new_node->prev = xx_next;
-	return (*h);
+	new_node->next = temp;
+	xx_next->next = new_node;
+	if (temp != NULL)
+		temp->prev = new_node;
+	return (new_node);
 }
