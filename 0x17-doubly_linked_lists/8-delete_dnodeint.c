3be12cbdd9704b0e2121c99b28098be815ae3be3
Rashnotech <rashnotech@gmail.com>
2023-06-16 23:10:27 +0000

    Fixed Error: Malloc failed returns

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,8 +19,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	temp = *head;
 	if (index == 0)
 	{	*head = temp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
+		free(temp);
+		return (1);
 	}
 	else
 	{
