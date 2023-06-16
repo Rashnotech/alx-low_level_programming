d491a42c4c96681283608c36f1383af5a2744819
Rashnotech <rashnotech@gmail.com>
2023-06-16 00:32:28 +0000

    Fixed the segmentation fault problem

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,33 +27,35 @@ int get_node_size(dlistint_t *head)
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *xx_next, *xx_prev;
-	unsigned int len, counter;
+	dlistint_t *new_node, *xx_next;
+	unsigned int len, counter = 0;
 
 	xx_next = *h;
-	xx_prev = NULL;
 	new_node = malloc(sizeof(dlistint_t));
 	if (!new_node)
 		return (NULL);
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
 	len = get_node_size(*h);
 	if (idx == 0)
-		add_dnodeint(h, n);
-	if (
+	{
+		new_node->next = *h;
+		(*h)->prev = new_node;
+	}
+	if (idx > len)
+		return (NULL);
 	else
 	{
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = NULL;
 		while (counter < (idx - 1))
 		{
-			xx_prev = xx_prev->next;
-			xx_next = xx_next->next->next;
 			counter++;
+			xx_next = xx_next->next;
 		}
+		new_node->next = xx_next->next;
+		new_node->prev = xx_next->prev;
+		xx_next->next = new_node;
 	}
-	new_node->next = xx_next;
-	xx_next->prev = new_node;
-	new_node->prev = xx_prev;
-	xx_next->next = new_node;
 	return (*h);
 }
+
