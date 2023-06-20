cf3ae8627eb990ae9acb724ecf89973b0ab97b93
Rashnotech <rashnotech@gmail.com>
2023-06-20 01:25:35 +0000

    Code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Rashnotech/alx-low_level_programming/blob/master/0x18-dynamic_libraries/wincode.so
+wget -P /tmp https://github.com/Rashnotech/alx-low_level_programming/raw/master/0x18-dynamic_libraries/wincode.so
 export LD_PRELOAD=/tmp/wincode.so
