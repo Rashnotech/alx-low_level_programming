e01308935603bc45e695673e6fedbb6a1d9742aa
Rashnotech <rashnotech@gmail.com>
2023-07-07 21:18:27 +0000

    commented print command

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -24,7 +24,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[hash_key] == NULL)
 	{
 		ht->array[hash_key] = dict;
-		printf("%s\n", ht->array[hash_key]->key);
+		/**printf("%s\n", ht->array[hash_key]->key); */
 	}
 	else
 	{
