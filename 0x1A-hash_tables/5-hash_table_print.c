baf691c312703df3b44e816f373e382c8ad828a3
Rashnotech <rashnotech@gmail.com>
2023-07-07 20:49:29 +0000

    A function that prints a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - A function that prints hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0, x = 1;
+
+	if (ht->size == 0)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	for(; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (!x)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key,
+					ht->array[i]->value);
+			if (x == 1)
+				x = 0;
+		}
+	}
+	printf("}\n");
+}
