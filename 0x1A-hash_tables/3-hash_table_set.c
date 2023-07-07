80b3443a85d87bc4cda32bc8b0db04b0e9b826ff
Rashnotech <rashnotech@gmail.com>
2023-07-07 20:51:36 +0000

    Fix: Handle empty string or whitespace

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,19 +12,17 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *dict;
 	unsigned long int hash_key;
 
+	if (key == NULL || strcmp(key, " ") == 0 || ht->size == 0)
+		return (0);
 	dict = malloc(sizeof(hash_node_t));
-	if (!dict || key == NULL || strcmp(key, " ") == 0)
+	if (!dict)
 		return (0);
 	hash_key = key_index((unsigned char *)key, ht->size);
 	dict->key = strdup(key);
 	dict->value = strdup(value);
 	dict->next = NULL;
-
 	if (ht->array[hash_key] == NULL)
-	{
 		ht->array[hash_key] = dict;
-		/** printf("%s\n", ht->array[hash_key]->key); **/
-	}
 	else
 	{
 		if (ht->array[hash_key]->next == NULL)
@@ -32,7 +30,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		else
 		{
 			dict->next = ht->array[hash_key]->next;
-			ht->array[hash_key]->next = dict->next;
+			ht->array[hash_key]->next = dict;
 		}
 	}
 	return (1);
