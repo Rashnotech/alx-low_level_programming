0b4f4f43c454feb7e86840f1009945ff0253b18d
Rashnotech <rashnotech@gmail.com>
2023-10-10 17:05:53 +0000

    Binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,7 @@
 
 /** Function prototype **/
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t, size_t);
 
 #endif
