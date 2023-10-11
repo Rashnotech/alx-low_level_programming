11406d76dceb7421598182dff37d643050489553
Rashnotech <rashnotech@gmail.com>
2023-10-11 22:34:51 +0000

    Interpolation search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+/**
+ * interpolation_search - a function that search for value
+ * @array: a pointer to the first element
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: an integer value searched otherwise -1
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t low = 0, high = size - 1, pos;
+
+	if (!array || size == 0)
+		return (-1);
+	while ((array[high] != array[low]) && (value >= array[low]) &&
+			(value <= array[high]))
+	{
+		pos = low + (((double)(high - low) /
+					(array[high] - array[low])) *
+				(value - array[low]));
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (array[pos] < value)
+			low = pos + 1;
+		else if (value < array[pos])
+			high = pos - 1;
+		else
+			return (pos);
+	}
+	if (value == array[low])
+		return (low);
+	return (-1);
+}
