0903a60a24c0ef46bbfddf4a9de520db7c33e53b
Rashnotech <rashnotech@gmail.com>
2023-10-12 13:27:28 +0000

    Fixed: betty error

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -18,10 +18,12 @@ int interpolation_search(int *array, size_t size, int value)
 					(array[high] - array[low])) *
 				(value - array[low]));
 		if (pos < size)
-			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+			printf("Value checked array[%lu] = [%d]\n", pos,
+					array[pos]);
 		else
 		{
-			 printf("Value checked array[%ld] is out of range\n", pos);
+			 printf("Value checked array[%ld] is out of range\n",
+					 pos);
 			 break;
 		}
 		if (array[pos] < value)
