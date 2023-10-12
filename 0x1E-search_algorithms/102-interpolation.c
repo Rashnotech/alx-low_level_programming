d45e669679ce0d1444c3982a91c3e85842ee2ee2
Rashnotech <rashnotech@gmail.com>
2023-10-12 13:25:56 +0000

    Fixed: catch(index out of range)

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,13 +12,18 @@ int interpolation_search(int *array, size_t size, int value)
 
 	if (!array || size == 0)
 		return (-1);
-	while ((array[high] != array[low]) && (value >= array[low]) &&
-			(value <= array[high]))
+	while (array[high] != array[low])
 	{
 		pos = low + (((double)(high - low) /
 					(array[high] - array[low])) *
 				(value - array[low]));
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		if (pos < size)
+			printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		else
+		{
+			 printf("Value checked array[%ld] is out of range\n", pos);
+			 break;
+		}
 		if (array[pos] < value)
 			low = pos + 1;
 		else if (value < array[pos])
