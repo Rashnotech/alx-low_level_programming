700cafed01ae857368e1c4f93a7d43c73d27ee7a
Rashnotech <rashnotech@gmail.com>
2023-10-13 22:30:39 +0000

    Fixed: check size

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -12,7 +12,7 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 	size_t step, prev = 0, i = 0;
 	listint_t *tmp;
 
-	if (!list)
+	if (!list || size == 0)
 		return (NULL);
 	step = sqrt(size);
 	prev = 0;
